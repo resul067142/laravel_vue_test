fd3ee3cec4f251bda4eccbb3d0744f17c8122f5e
resul <resul_067142@hotmail.com>
2024-10-13 17:19:00 +0300

    Ana sayfa mobil uygum çalışaması yapıldı

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-6">
+        <div class="col-12 col-md-6">
             <div class="card">
                 <div class="card-body">
                     <div v-if="!yarismaBasladi" class="row">
@@ -25,7 +25,7 @@
                             <div class="text-center" style="font-size: 24px; margin-bottom: 10px">
                                 {{secilenSoruSirasi + 1}} / {{sorular.length}}
                             </div>
-                            <div style="background-color: darkcyan; color: white; min-height: 50px;">
+                            <div class="p-2" style="background-color: darkcyan; color: white; min-height: 50px;">
                                 {{secilenSoru.soru}}
                             </div>
 
@@ -37,12 +37,12 @@
                                  :class="'secenek' +  (secilenSoru.secilen_cevap && secilenSoru.secilen_cevap === 2 ? ' secilen' : '')">
                                 2 - {{secilenSoru.secenek_2}}
                             </div>
-                            <div  @click="sec(3)"
-                                  :class="'secenek' +  (secilenSoru.secilen_cevap && secilenSoru.secilen_cevap === 3 ? ' secilen' : '')">
+                            <div @click="sec(3)"
+                                 :class="'secenek' +  (secilenSoru.secilen_cevap && secilenSoru.secilen_cevap === 3 ? ' secilen' : '')">
                                 3 - {{secilenSoru.secenek_3}}
                             </div>
-                            <div  @click="sec(4)"
-                                  :class="'secenek' +  (secilenSoru.secilen_cevap && secilenSoru.secilen_cevap === 4 ? ' secilen' : '')">
+                            <div @click="sec(4)"
+                                 :class="'secenek' +  (secilenSoru.secilen_cevap && secilenSoru.secilen_cevap === 4 ? ' secilen' : '')">
                                 4 - {{secilenSoru.secenek_4}}
                             </div>
                             <div v-if="skorKaydedildi">
@@ -54,7 +54,7 @@
                                 </div>
                             </div>
 
-                            <div style="margin-top: 10px; margin-bottom: 10px; text-align: center">
+                            <div class="text-center mt-2 mb-2">
                                 <button class="btn btn-info m-1" v-if="secilenSoruSirasi != 0" type="button" @click="oncekiSoru"> {{ '<' }} </button>
                                 <button class="btn btn-info m-1" v-if="secilenSoruSirasi < sorular.length - 1" type="button" @click="sonrakiSoru"> {{ '>' }} </button>
                             </div>
@@ -62,34 +62,33 @@
                     </div>
                     <div class="row">
                         <div class="col-12 m-2 text-center">
-                            <button class="btn btn-success" style="width: 250px" @click="bitir" :disabled="bittiMi" v-if="yarismaBasladi">Bitir</button>
+                            <button class="btn btn-success" style="width: 100%;" @click="bitir" :disabled="bittiMi" v-if="yarismaBasladi">Bitir</button>
                             <div v-if="skorKaydedildi" class="mt-2 mb-2" style="font-size: 28px;">
                                 {{yuzdeDogruCevap}}%
                             </div>
                             <button @click="yenidenOyna" class="btn btn-primary" v-if="skorKaydedildi">Yeniden Oyna</button>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-6">
+        <div class="col-12 col-md-6 mt-3 mt-md-0">
             <div class="card">
                 <div class="card-body">
                     <table class="table">
                         <thead>
-                            <tr>
-                                <th>Fotoğraf</th>
-                                <th>Ad</th>
-                                <th>Puan</th>
-                            </tr>
+                        <tr>
+                            <th>Fotoğraf</th>
+                            <th>Ad</th>
+                            <th>Puan</th>
+                        </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="score in scores">
-                                <td><img class="rounded-circle" :src="'uploads/' + score.img_url" style="height: 80px; width: 80px" alt=""/></td>
-                                <td>{{ score.name }}</td>
-                                <td>{{ score.score }}</td>
-                            </tr>
+                        <tr v-for="score in scores">
+                            <td><img class="rounded-circle img-fluid" :src="'uploads/' + score.img_url" :style="imgStyle" alt=""/></td>
+                            <td>{{ score.name }}</td>
+                            <td>{{ score.score }}</td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
@@ -97,6 +96,7 @@
         </div>
     </div>
 </template>
+
 <script>
 import axios from "axios";
 
@@ -117,8 +117,14 @@ export default {
     },
     computed: {
         bittiMi() {
-            return this.sorular && !this.sorular.every(soru => soru.hasOwnProperty('secilen_cevap'))
-
+            return this.sorular && !this.sorular.every(soru => soru.hasOwnProperty('secilen_cevap'));
+        },
+        imgStyle() {
+            return {
+                width: '100px',
+                height: '100px',
+                objectFit: 'cover'
+            };
         }
     },
     methods: {
@@ -126,59 +132,53 @@ export default {
             this.photo = event.target.files[0];
         },
         yenidenOyna() {
-            this.yarismaBasladi = false
-            this.skorKaydedildi = false
-            this.name = null
+            this.yarismaBasladi = false;
+            this.skorKaydedildi = false;
+            this.name = null;
         },
         start() {
-            this.secilenSoruSirasi = 0
+            this.secilenSoruSirasi = 0;
             axios.get('/api/sorular').then(res => {
-                console.log('res => ',res);
-                this.sorular = res.data
-                this.secilenSoru = this.sorular[0]
-                this.yarismaBasladi = true
-            })
-            console.log('Products component created.')
+                this.sorular = res.data;
+                this.secilenSoru = this.sorular[0];
+                this.yarismaBasladi = true;
+            });
         },
         oncekiSoru() {
-            this.secilenSoruSirasi--
-            this.secilenSoru = this.sorular[this.secilenSoruSirasi]
+            this.secilenSoruSirasi--;
+            this.secilenSoru = this.sorular[this.secilenSoruSirasi];
         },
         sonrakiSoru() {
-            this.secilenSoruSirasi++
-            this.secilenSoru = this.sorular[this.secilenSoruSirasi]
+            this.secilenSoruSirasi++;
+            this.secilenSoru = this.sorular[this.secilenSoruSirasi];
         },
         sec(secenek){
-            if (!this.skorKaydedildi)
-            {
-                this.sorular[this.secilenSoruSirasi].secilen_cevap = secenek
+            if (!this.skorKaydedildi) {
+                this.sorular[this.secilenSoruSirasi].secilen_cevap = secenek;
             }
         },
         bitir() {
-            const soruSayisi = this.sorular.length
-            const dogruCevapSayisi = this.sorular.filter(soru => soru.dogru_cevap === soru.secilen_cevap).length
-            this.yuzdeDogruCevap = Math.ceil(dogruCevapSayisi / soruSayisi* 100)
+            const soruSayisi = this.sorular.length;
+            const dogruCevapSayisi = this.sorular.filter(soru => soru.dogru_cevap === soru.secilen_cevap).length;
+            this.yuzdeDogruCevap = Math.ceil(dogruCevapSayisi / soruSayisi * 100);
 
             const formData = new FormData();
             formData.append('photo', this.photo);
             formData.append('name', this.name);
             formData.append('score', this.yuzdeDogruCevap);
 
-
-            axios.post('/api/scores', formData).then(res => {
-                this.skorKaydedildi = true
-
+            axios.post('/api/scores', formData).then(() => {
+                this.skorKaydedildi = true;
                 axios.get('/api/scores').then(res => {
-                    this.scores = res.data
-                })
-            })
-
+                    this.scores = res.data;
+                });
+            });
         }
     },
     mounted() {
         axios.get('/api/scores').then(res => {
-            this.scores = res.data
-        })
+            this.scores = res.data;
+        });
     }
-}
+};
 </script>
