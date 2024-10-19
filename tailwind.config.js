5c0de3ff1980af7c12d59a0601d66dcb9621fe3b
r <resul_067142@hotmail.com>
2024-10-19 03:57:30 +0300

    Sunucu Geliştirme ortamı Uyumsuzluk hatası giderildi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-
